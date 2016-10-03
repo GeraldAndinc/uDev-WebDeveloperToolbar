c61d7d8cfa50cd6a1e0b118505bf048df6c1ca2c
Gerald Andinc <gerandinc@gmail.com>
2016-10-04 00:24:32 +0200

    Add files via upload

---
+++ about/javascript/about.js
@@ -0,0 +1,18 @@
+var WebDeveloper = WebDeveloper || {}; // eslint-disable-line no-use-before-define
+
+WebDeveloper.Generated = WebDeveloper.Generated || {};
+
+// Initializes the page with data
+WebDeveloper.Generated.initialize = function(data, locale)
+{
+  var name = locale.extensionName;
+
+  $("title").text(locale.about + " " + name);
+  $("h1").text(name);
+  $("#description").text(locale.extensionDescription);
+  $("#follow-on-appnet").text(locale.followOnAppNet);
+  $("#follow-on-twitter").text(locale.followOnTwitter);
+  $("#author").text(locale.author);
+  $("#version").text(locale.version);
+  $("#build-date").text(locale.buildDate);
+};
